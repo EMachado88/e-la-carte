c8700181060c86b9f1e67aa3b1ebfffefa7a8795
Emanuel Machado <emachado@emachado.pt>
2019-11-09 17:46:25 +0000

    Initial setup

---
+++ README.md
@@ -0,0 +1,29 @@
+# eLaCarte
+
+> App for generating and maintaining monthly meal lists.
+
+Tanya is the manager of her school system’s cafeterias.  Every month she needs to come up with school lunches for the next month.  The school lunches should have a variety of different mains, sides, and desserts.
+
+Write Tanya a program that will put together her lunches for her.
+1. Come up with a list of mains, sides, and desserts
+2. Be able to generate a list of meals.
+3. The meals should be unique.
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ layouts/default.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      absolute
+      app
+    >
+      <span class="d-flex">
+        <v-icon x-small class="mr-1">mdi-copyright</v-icon> 2019 emachado. All rights reserved.
+      </span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        }
+      ],
+      title: 'eALaCarte'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+footer {
+  font-size: 12px;
+}
+</style>
