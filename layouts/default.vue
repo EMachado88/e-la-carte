a4b6cce2a697e2e7ae3818c75d7d2e190e45997b
Emanuel Machado <emachado@emachado.pt>
2019-11-10 16:48:18 +0000

    Removed navigation drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,9 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
     </v-app-bar>
@@ -36,7 +13,7 @@
       </v-container>
     </v-content>
     <v-footer
-      absolute
+      fixed
       app
     >
       <span class="d-flex">
